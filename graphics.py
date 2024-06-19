ade41219a3ed10ea3d5faa3a27897f77fa2b8d37
Axelandrovitch <axel.a.hetru@gmail.com>
2024-06-19 13:32:52 +0200

    add Maze class. Methods to be added

--- graphics.py
+++ graphics.py
@@ -90,6 +90,20 @@ class Cell:
         center_to_center = Line(center_1, center_2)
         self._win.draw_line(line=center_to_center, fill_color=color)
 
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.cells = []
+        self.win = win
+
+    def _create_cells(self):
+        
+
 
 #helper functions
 
